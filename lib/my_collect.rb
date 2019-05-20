91cf307fd8397a8733a75ea3c27ee6b8b704345e
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-20 23:03:26 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(argum)
+  counter = 0
+  return_arg = []
+  while counter < argum.length
+    new_ = yield(argum[counter])
+    return_arg << new_
+    counter += 1
+  end
+  return return_arg
+end
